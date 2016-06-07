83bb450232981dcbd643ff95afd8ce8f428d1f83
f18n <firman18@outlook.com>
2016-06-07 09:35:36 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,15 +3,15 @@ session_start();
 include_once("src/Google_Client.php");
 include_once("src/contrib/Google_Oauth2Service.php");
 ######### edit details ##########
-$clientId = "613987527036-1eapc1u94mfgsperpv0vo9gjn5t72ses.apps.googleusercontent.com"; //Google CLIENT ID
-$clientSecret = 'zIs6_eAG4x5I5J22NvMTnCP0'; //Google CLIENT SECRET
-$redirectUrl = "http://localhost/app/loging/";  //return url (url to script)
-$homeUrl = 'http://www.loging.com';  //return to home
+$clientId = "blabla"; //Google CLIENT ID
+$clientSecret = 'blabla'; //Google CLIENT SECRET
+$redirectUrl = "blabla";  //return url (url to script) or place your folder script
+$homeUrl = 'blabla';  //return to home
 
 ##################################
 
 $gClient = new Google_Client();
-$gClient->setApplicationName('masuku');
+$gClient->setApplicationName('blabla'); //can set or not
 $gClient->setClientId($clientId);
 $gClient->setClientSecret($clientSecret);
 $gClient->setRedirectUri($redirectUrl);
